31f32185fe71fa59fd33a0901ba7aca36a070fb8
triest <triest21@gmail.com>
2019-03-18 22:51:28 +0300

    prepare mysql

---
+++ PlanetsDB.php
@@ -0,0 +1,88 @@
+<?php
+
+class PlanetsApi
+{
+    private $host = "";
+    private $password = "";
+    private $login = "";
+    private $databese = "";
+
+
+    public function insertDataTodataBese()
+    {
+        try {
+            $dbh = new PDO('mysql:host='.$this->host.';dbname='.$this->databese,$this->login,$this->password);
+            $stmt = $dbh->prepare("INSERT INTO REGISTRY (name, value) VALUES (:name, :value)");
+        } catch (PDOException $e) {
+            print "Error!: ".$e->getMessage()."<br/>";
+            die();
+        }
+
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getHost(): string
+    {
+        return $this->host;
+    }
+
+    /**
+     * @param string $host
+     */
+    public function setHost(string $host): void
+    {
+        $this->host = $host;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogin(): string
+    {
+        return $this->login;
+    }
+
+    /**
+     * @param string $login
+     */
+    public function setLogin(string $login): void
+    {
+        $this->login = $login;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDatabese(): string
+    {
+        return $this->databese;
+    }
+
+    /**
+     * @param string $databese
+     */
+    public function setDatabese(string $databese): void
+    {
+        $this->databese = $databese;
+    }
+
+
